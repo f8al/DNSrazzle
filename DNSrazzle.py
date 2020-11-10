5ca0c5e0cea821b44ef9f4952a7ad95f28190980
f8al <f8al.err0r@gmail.com>
2020-11-10 13:09:10 -0600

    initial commit

---
+++ DNSrazzle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+# -*- coding: utf-8 -*-
+
+#    DNSRecon
+#
+#    Copyright (C) 2020  Carlos Perez
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; Applies version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+__version__ = '0.0.1'
+__author__ = 'securityshrimp @securityshrimp
+
+
+
+'''
+ ______  __    _ _______ ______   _______ _______ _______ ___     _______ 
+|      ||  |  | |       |    _ | |   _   |       |       |   |   |       |
+|  _    |   |_| |  _____|   | || |  |_|  |____   |____   |   |   |    ___|
+| | |   |       | |_____|   |_||_|       |____|  |____|  |   |   |   |___ 
+| |_|   |  _    |_____  |    __  |       | ______| ______|   |___|    ___|
+|       | | |   |_____| |   |  | |   _   | |_____| |_____|       |   |___ 
+|______||_|  |__|_______|___|  |_|__| |__|_______|_______|_______|_______|
+'''
