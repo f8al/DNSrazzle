8fee1efa0a3f58efc2ae388367f8838d8d761b99
f8al <f8al.err0r@gmail.com>
2020-11-18 11:14:12 -0600

    removed unused line in compare_screenshot()

--- DNSrazzle.py
+++ DNSrazzle.py
@@ -164,7 +164,6 @@ def compare_screenshots(imageA, imageB):
         # compute the Structural Similarity Index (SSIM) between the two
         # images, ensuring that the difference image is returned
         (score, diff) = compare_ssim(grayA, grayB, full=True)
-        diff = (diff * 255).astype("uint8")
         #print("SSIM: {}".format(score))
         rounded_score = round(score, 2)
 
@@ -177,29 +176,6 @@ def compare_screenshots(imageA, imageB):
     except cv2.error as exception:
             print_error(f"Unable to compare screenshots.  One or more of the screenshots are missing!")
 
-    """
-    # threshold the difference image, followed by finding contours to
-    # obtain the regions of the two input images that differ
-    thresh = cv2.threshold(diff, 0, 255,
-                           cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    # loop over the contours
-    for c in cnts:
-        # compute the bounding box of the contour and then draw the
-        # bounding box on both input images to represent where the two
-        # images differ
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(image_A, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        cv2.rectangle(image_B, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    # show the output images
-    cv2.imshow("Original", image_A)
-    cv2.imshow("Modified", image_B)
-    cv2.imshow("Diff", diff)
-    cv2.imshow("Thresh", thresh)
-    #cv2.waitKey(0)
-    """
 
 def portscan(domain, out_dir):
     print_status(f"Running nmap on {domain}")
