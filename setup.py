56d74467791497d085c8130360727cc984d55236
f8al <f8al.err0r@gmail.com>
2020-11-20 21:30:54 -0600

    incremented version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("docs/README.md", "r") as fh:
 
 setup(
 	name='DNSrazzle',
-	version='0.0.9',
+	version='0.1.0',
     license='Apache',
 	author='SecurityShrimp',
 	author_email='@securityshrimp',
