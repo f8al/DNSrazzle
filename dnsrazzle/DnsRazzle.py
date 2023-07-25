d208d4c5a8f6f8c94693fc3159a94a73603b40e3
SecurityShrimp <f8al.err0r@gmail.com>
2023-07-25 10:58:35 -0500

    Update DnsRazzle.py
    added file header.  bumped version

--- dnsrazzle/DnsRazzle.py
+++ dnsrazzle/DnsRazzle.py
@@ -1,3 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+ ______  __    _ _______ ______   _______ _______ _______ ___     _______
+|      ||  |  | |       |    _ | |   _   |       |       |   |   |       |
+|  _    |   |_| |  _____|   | || |  |_|  |____   |____   |   |   |    ___|
+| | |   |       | |_____|   |_||_|       |____|  |____|  |   |   |   |___
+| |_|   |  _    |_____  |    __  |       | ______| ______|   |___|    ___|
+|       | | |   |_____| |   |  | |   _   | |_____| |_____|       |   |___
+|______||_|  |__|_______|___|  |_|__| |__|_______|_______|_______|_______|
+
+
+Generate, resolve, and compare domain variations to detect typosquatting,
+phishing, and brand impersonation
+
+Copyright 2023 SecurityShrimp
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
+
+
+__version__ = '1.5.0'
+__author__ = 'SecurityShrimp'
+__twitter__ = '@securityshrimp'
+__email__ = 'securityshrimp@proton.me'
+
 from .BrowserUtil import screenshot_domain
 from .NetUtil import run_portscan, run_recondns
 from .VisionUtil import compare_screenshots
