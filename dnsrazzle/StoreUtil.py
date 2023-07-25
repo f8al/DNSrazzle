d3f3c0b2ae830c1900746472b3f3db6d69164695
SecurityShrimp <f8al.err0r@gmail.com>
2023-07-25 10:48:24 -0500

    Create StoreUtil.py
    added util file for creating and writing data to disk

---
+++ dnsrazzle/StoreUtil.py
@@ -0,0 +1,147 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+ ______  __    _ _______ ______   _______ _______ _______ ___     _______
+|      ||  |  | |       |    _ | |   _   |       |       |   |   |       |
+|  _    |   |_| |  _____|   | || |  |_|  |____   |____   |   |   |    ___|
+| | |   |       | |_____|   |_||_|       |____|  |____|  |   |   |   |___
+| |_|   |  _    |_____  |    __  |       | ______| ______|   |___|    ___|
+|       | | |   |_____| |   |  | |   _   | |_____| |_____|       |   |___
+|______||_|  |__|_______|___|  |_|__| |__|_______|_______|_______|_______|
+
+
+Generate, resolve, and compare domain variations to detect typosquatting,
+phishing, and brand impersonation
+
+Copyright 2023 SecurityShrimp
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
+
+
+__version__ = '1.5.0'
+__author__ = 'SecurityShrimp'
+__twitter__ = '@securityshrimp'
+nameserver = '1.1.1.1'
+
+import sqlite3
+import json
+import re
+
+"""
+this file contains all of the methods for writing output to disk in a variety of ways.
+
+def create_db(db):
+    """
+    Function will create the specified database if not present and it will create
+    the table needed for storing the data returned by the modules.
+    """
+
+    # Connect to the DB
+    con = sqlite3.connect(db)
+
+    # Create SQL Queries to be used in the script
+    make_table = """CREATE TABLE data (
+    serial integer  Primary Key Autoincrement,
+    domain TEXT(256),
+    type TEXT(8),
+    name TEXT(32),
+    address TEXT(32),
+    target TEXT(32),
+    port TEXT(8),
+    text TEXT(256),
+    zt_dns TEXT(32)
+    )"""
+
+    # Set the cursor for connection
+    con.isolation_level = None
+    cur = con.cursor()
+
+    # Connect and create table
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='data';")
+    if cur.fetchone() is None:
+        cur.execute(make_table)
+        con.commit()
+    else:
+        pass
+
+  def write_json(jsonfile, data, scan_info):
+    """
+    Function to write DNS Records SOA, PTR, NS, A, AAAA, MX, TXT, SPF and SRV to
+    JSON file.
+    """
+    scaninfo = {"type": "ScanInfo", "arguments": scan_info[0], "date": scan_info[1]}
+    data.insert(0, scaninfo)
+    json_data = json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+    write_to_file(json_data, jsonfile)
+
+
+def write_db(db, data):
+    """
+    Function to write DNS Records SOA, PTR, NS, A, AAAA, MX, TXT, SPF and SRV to
+    DB.
+    """
+
+    con = sqlite3.connect(db)
+    # Set the cursor for connection
+    con.isolation_level = None
+    cur = con.cursor()
+
+    # Normalize the dictionary data
+    for n in data:
+
+        if re.match(r'PTR|^[A]$|AAAA', n['type']):
+            query = 'insert into data( domain, type, name, address ) ' + \
+                    'values( "%(domain)s", "%(type)s", "%(name)s","%(address)s" )' % n
+
+        elif re.match(r'NS$', n['type']):
+            query = 'insert into data( domain, type, name, address ) ' + \
+                    'values( "%(domain)s", "%(type)s", "%(target)s", "%(address)s" )' % n
+
+        elif re.match(r'SOA', n['type']):
+            query = 'insert into data( domain, type, name, address ) ' + \
+                    'values( "%(domain)s", "%(type)s", "%(mname)s", "%(address)s" )' % n
+
+        elif re.match(r'MX', n['type']):
+            query = 'insert into data( domain, type, name, address ) ' + \
+                    'values( "%(domain)s", "%(type)s", "%(exchange)s", "%(address)s" )' % n
+
+        elif re.match(r'TXT', n['type']):
+            query = 'insert into data( domain, type, text) ' + \
+                    'values( "%(domain)s", "%(type)s","%(strings)s" )' % n
+
+        elif re.match(r'SPF', n['type']):
+            query = 'insert into data( domain, type, text) ' + \
+                    'values( "%(domain)s", "%(type)s","%(strings)s" )' % n
+
+        elif re.match(r'SRV', n['type']):
+            query = 'insert into data( domain, type, name, target, address, port ) ' + \
+                    'values( "%(domain)s", "%(type)s", "%(name)s" , "%(target)s", "%(address)s" ,"%(port)s" )' % n
+
+        elif re.match(r'CNAME', n['type']):
+            query = 'insert into data( domain, type, name, target ) ' + \
+                    'values( "%(domain)s", "%(type)s", "%(name)s" , "%(target)s" )' % n
+
+        else:
+            # Handle not common records
+            t = n['type']
+            del n['type']
+            record_data = "".join(['%s=%s,' % (key, value) for key, value in n.items()])
+            records = [t, record_data]
+            query = "insert into data(domain,type,text) values (\"%(domain)\", '" + \
+                    records[0] + "','" + records[1] + "')"
+
+        # Execute Query and commit
+        cur.execute(query)
+        con.commit()
